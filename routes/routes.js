1c7d71d9bb726d430c9351e300d4f2e13b66dfbb
Pratham <Prathamgarg456@gmail.com>
2022-10-02 21:29:17 -0400

    removed

--- routes/routes.js
+++
@@ -1,9 +0,0 @@
-const { home, about, contactus, info } = require('../controllers/controllers.js')
-const express = require('express')
-const router = express.Router()
-
-router.get('/home', home)
-router.get('/about', about)
-router.get('/contactUS', contactus)
-router.get('/info', info)
-module.exports = router
